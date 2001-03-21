667e7eb03413ef89bc6d5b6a96b7f676c6159884
fenk <fenk>
2001-03-21 14:35:17 +0000

    (bbdb-prompt-for-create): fix a bug with GNU Emacs.

--- lisp/bbdb-com.el
+++ lisp/bbdb-com.el
@@ -20,7 +20,7 @@
 ;;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
 ;;
-;; $Id: bbdb-com.el,v 1.102 2001/03/19 12:37:59 fenk Exp $
+;; $Id: bbdb-com.el,v 1.103 2001/03/21 14:35:17 fenk Exp $
 ;;
 
 (require 'bbdb)
@@ -2992,9 +2992,13 @@ proceed the processing of records."
       (when (not (integerp bbdb-offer-to-create))
         (setq prompt (format "%s is not in the db; add? (y,!,n,s,q,?)"
                              (or (car bbdb-address) (cadr bbdb-address))))
-        (while (not (key-press-event-p
-                     (setq event (next-command-event nil prompt)))))
-        (setq bbdb-offer-to-create (char-int (event-to-character event))))
+        (while (not event)
+          (setq event (read-key-sequence prompt))
+          (if (featurep 'xemacs)
+              (setq event (char-int (event-to-character event)))
+            (setq event (if (stringp event) (aref event 0)))))
+          
+        (setq bbdb-offer-to-create event))
       
       (cond ((eq bbdb-offer-to-create (char-int ?y))
              (setq bbdb-offer-to-create old-offer-to-create)
