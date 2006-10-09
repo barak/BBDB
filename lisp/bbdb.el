740c1c5dcf20d139bdec31644c55fd2bdee82ec3
fenk <fenk>
2006-10-09 22:57:28 +0000

    Fixed call to replace-in-string in bbdb-format-record-one-line-notes.

--- lisp/bbdb.el
+++ lisp/bbdb.el
@@ -35,7 +35,7 @@
 ;;; |  information plus state information about how you have BBDB set up.    |
 ;;;  ------------------------------------------------------------------------
 ;;;
-;;; $Id: bbdb.el,v 1.229 2006/05/25 23:19:53 fenk Exp $
+;;; $Id: bbdb.el,v 1.230 2006/10/09 22:57:28 fenk Exp $
 
 (require 'timezone)
 (eval-when-compile (require 'cl))
@@ -65,7 +65,7 @@
  )
 
 (defconst bbdb-version "2.35")
-(defconst bbdb-version-date "$Date: 2006/05/25 23:19:53 $")
+(defconst bbdb-version-date "$Date: 2006/10/09 22:57:28 $")
 
 (defcustom bbdb-gui (if (fboundp 'display-color-p) ; Emacs 21
                         (display-color-p)
@@ -1539,7 +1539,7 @@ formatted and inserted into the current buffer.  This is used by
   "Insert formatted notes for one-line display.
 Line breaks will be removed and white space trimmed."
   (let ((start (point)))
-    (insert (replace-in-string notes "[\r\n\t ]+" " "))
+    (insert (bbdb-replace-in-string notes "[\r\n\t ]+" " "))
     (put-text-property start (point) 'bbdb-field (list 'notes notes))))
 
 (defun bbdb-format-record-layout-one-line (layout record field-list)
